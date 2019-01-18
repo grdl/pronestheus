441082073fc788ebb330bf4b83acd830a3c87bde
Greg Dlugoszewski <greg@gridarrow.com>
2019-01-18 16:10:35 +0100

    Add exposing nest metrics

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gitlab.com/grdl/pronestheus
 require (
 	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
 	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/prometheus/client_golang v0.9.2
 	github.com/prometheus/common v0.0.0-20181126121408-4724e9255275
 	github.com/sirupsen/logrus v1.3.0 // indirect
--- pronestheus.go
+++ pronestheus.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"errors"
+	"encoding/json"
+
+	"github.com/pkg/errors"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 
@@ -20,13 +22,36 @@ var (
 	nestApiToken  = kingpin.Flag("nest-api-token", "The authorization token for Nest API").Required().String()
 )
 
+var nestLabels = []string{"id", "name"}
+
 var (
-	nestUp = prometheus.NewDesc(
-		"pns_nest_up",
-		"Was talking to Nest API successful.",
-		nil, nil,
-	)
+	nestUp       = prometheus.NewDesc("nest_up", "Was talking to Nest API successful.", nil, nil)
+	nestTemp     = prometheus.NewDesc("nest_current_temp", "Current ambient temperature.", nestLabels, nil)
+	nestTarget   = prometheus.NewDesc("nest_target_temp", "Current target temperature.", nestLabels, nil)
+	nestHumidity = prometheus.NewDesc("nest_humidity", "Current inside humidity.", nestLabels, nil)
+	nestHeating  = prometheus.NewDesc("nest_heating", "Is thermostat heating.", nestLabels, nil)
+	nestLeaf     = prometheus.NewDesc("nest_leaf", "Is thermostat set to energy-saving temperature.", nestLabels, nil)
+	nestSunlight = prometheus.NewDesc("nest_sunlight", "Is thermostat in direct sunlight.", nestLabels, nil)
 )
+
+// Thermostat stores thermostat readings received from Nest API
+type Thermostat struct {
+	Id          string  `json:"device_id"`
+	Name        string  `json:"name"`
+	Temperature float64 `json:"ambient_temperature_c"`
+	Target      float64 `json:"target_temperature_c"`
+	Humidity    float64 `json:"humidity"`
+	HVACState   string  `json:"hvac_state"`
+	Leaf        bool    `json:"has_leaf"`
+	Sunlight    bool    `json:"sunlight_correction_active"`
+}
+
+//Weather stores weather data received from Open Weather Map API
+type Weather struct {
+	Temperature float64 `json:"temp"`
+	Humidity    float64 `json:"humidity"`
+	Pressure    float64 `json:"pressure"`
+}
 
 type NestCollector struct {
 }
@@ -38,18 +63,45 @@ func (c NestCollector) Describe(ch chan<- *prometheus.Desc) {
 
 // Implements prometheus.Collector
 func (c NestCollector) Collect(ch chan<- prometheus.Metric) {
-	thermostats, err := getNestData()
+	thermostats, err := getNestReadings()
 	if err != nil {
 		ch <- prometheus.MustNewConstMetric(nestUp, prometheus.GaugeValue, 0)
+		log.Error(err)
 		return
 	}
 
 	ch <- prometheus.MustNewConstMetric(nestUp, prometheus.GaugeValue, 1)
 
+	for _, therm := range thermostats {
+		labels := []string{therm.Id, therm.Name}
+
+		ch <- prometheus.MustNewConstMetric(nestTemp, prometheus.GaugeValue, therm.Temperature, labels...)
+		ch <- prometheus.MustNewConstMetric(nestTarget, prometheus.GaugeValue, therm.Target, labels...)
+		ch <- prometheus.MustNewConstMetric(nestHumidity, prometheus.GaugeValue, therm.Humidity, labels...)
+
+		if therm.HVACState == "heating" {
+			ch <- prometheus.MustNewConstMetric(nestHeating, prometheus.GaugeValue, 1, labels...)
+		} else {
+			ch <- prometheus.MustNewConstMetric(nestHeating, prometheus.GaugeValue, 0, labels...)
+		}
+
+		if therm.Leaf {
+			ch <- prometheus.MustNewConstMetric(nestLeaf, prometheus.GaugeValue, 1, labels...)
+		} else {
+			ch <- prometheus.MustNewConstMetric(nestLeaf, prometheus.GaugeValue, 0, labels...)
+		}
+
+		if therm.Sunlight {
+			ch <- prometheus.MustNewConstMetric(nestSunlight, prometheus.GaugeValue, 1, labels...)
+		} else {
+			ch <- prometheus.MustNewConstMetric(nestSunlight, prometheus.GaugeValue, 0, labels...)
+		}
+	}
+
 	log.Info(thermostats)
 }
 
-func getNestData() (result string, err error) {
+func getNestReadings() (thermostats []Thermostat, err error) {
 
 	req, _ := http.NewRequest("GET", *nestApiURL, nil)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *nestApiToken))
@@ -69,17 +121,29 @@ func getNestData() (result string, err error) {
 
 	res, err := customClient.Do(req)
 	if err != nil {
-		return "", err
+		return nil, errors.Wrap(err, "Calling Nest API failed")
 	}
+
+	// TODO: should return error if response is not 2xx
 
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return "", err
+		return nil, errors.Wrap(err, "Reading Nest API response failed")
 	}
 
-	return string(body), nil
+	var data map[string]Thermostat
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, errors.Wrap(err, "Unmarshalling Nest API response failed")
+	}
+
+	for _, thermostat := range data {
+		thermostats = append(thermostats, thermostat)
+	}
+
+	return thermostats, nil
 }
 
 func main() {

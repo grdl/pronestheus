7e424ae80a25cd3d9fd177843dc0817c98e504a4
Alex Klyubin <klyubin@gmail.com>
2022-12-25 16:41:59 -0800

    Updated tests

--- pkg/collectors/nest/nest_test.go
+++ pkg/collectors/nest/nest_test.go
@@ -21,6 +21,7 @@ func TestServerResponses(t *testing.T) {
 			wantErr: nil,
 			want: &Thermostat{
 				ID:           "enterprises/PROJECT_ID/devices/DEVICE_ID",
+				Room:         "Living Room",
 				Label:        "Custom Name",
 				AmbientTemp:  float64(20.23999),
 				SetpointTemp: float64(19.17838),
